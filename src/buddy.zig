f97cceaff46b7f9af10d5213dc1201ee55e3740c
Mason Jones <20848827+smj-edison@users.noreply.github.com>
2025-10-20 17:47:09 -0700

    WIP

--- src/buddy.zig
+++ src/buddy.zig
@@ -5,66 +5,107 @@
 const std = @import("std");
 const math = std.math;
 const assert = std.debug.assert;
+const Allocator = std.mem.Allocator;
 
-const Config = struct {};
+const FreeList = std.ArrayListUnmanaged(usize);
 
-pub fn metadata_backing_size(order: u6) usize {
-    // Explanation:
-    // `@as(usize, 1) << (order - 1)`: This is the number of order 0 blocks.
-    // For example, if you had order 4, you'd have:
-    //  * order 3 = 1 block
-    //  * order 2 = 2 blocks
-    //  * order 1 = 4 blocks
-    //  * order 0 = 8 blocks (the one in question)
-    //
-    // `* 2`: all the blocks of order 1 or higher fit within the size of the
-    // order 0 blocks
-    // `* 2`: two bits needed to represent left and right half of buddy
-    // `/ @sizeOf(usize) * 8`: we use one bit to track each block. We use words
-    // as the backing, so this returns the number of words needed to track
-    // all the blocks of all the orders.
-    return (@as(usize, 1) << (order - 1)) * 2 * 2 / (@sizeOf(usize) * 8);
-}
-
-/// `metadata_backing`'s size can be determined with `metadata_backing_size`.
-pub fn BuddyUnmanaged(max_order: u6, config: Config) type {
-    const metadata_word_size = metadata_backing_size(max_order);
-
-    const MetadataBitSet = std.bit_set.ArrayBitSet(usize, metadata_word_size);
+/// Dependant on parent allocator to resize the internal free lists
+pub fn BuddyUnmanaged(max_order: comptime_int) type {
+    const total_block_count = 1 << max_order;
+    const AllocTracking = std.bit_set.ArrayBitSet(usize, total_block_count / (@sizeOf(usize) * 8));
 
     return struct {
-        metadata: MetadataBitSet,
+        // TODO: make this one data structure
+        next_free: [max_order]FreeList,
+        alloc_tracking: AllocTracking,
 
         const Self = @This();
 
-        pub fn init(metadata_backing: []usize) Self {
-            assert(metadata_backing.len == metadata_word_size);
+        fn init(allocator: Allocator, initial_capacity: usize) Self {
+            var new_alloc: Self = .{
+                .next_free = undefined,
+            };
 
-            @memset(metadata_backing, 0);
+            for (0..max_order) |i| {
+                new_alloc.next_free[i] = FreeList.initCapacity(allocator, initial_capacity);
+            }
+
+            new_alloc.next_free[max_order - 1] = 0;
         }
 
-        pub fn alloc(order: u6) error.OutOfMemory!usize {}
+        pub fn alloc(self: Self, allocator: Allocator, requested_order: u6) error.OutOfMemory!usize {
+            // look for an open block
+            var open_index = undefined;
+            var open_order = requested_order;
+            while (open_order < max_order) : (open_order += 1) {
+                if (self.next_free[open_order].pop()) |open| {
+                    open_index = open;
+                    break;
+                }
+            } else return error.OutOfMemory;
 
-        fn alloc_impl() error.OutOfMemory!usize {}
+            // split blocks (if needed).
+            while (open_order > requested_order) : (open_order -= 1) {
+                self.next_free[open_order - 1].append(allocator, open_index + get_order_size(open_order - 1));
+                // Lower half is implicitly passed along `open_index`, since
+                // the lower block index stays the same as it descends
+            }
 
-        pub fn free(index: usize, order: u6) void {}
+            return open_index;
+        }
 
-        const max_usize = @as(usize, math.maxInt(usize));
-        fn order_bit_index(order: u6) usize {
-            // Explanation:
-            // The high-level mechanism of this bit trickery is sequentially setting bits,
-            // in highest to lowest order. For example, say our max order is 4, and the
-            // passed in order is one of the following:
-            // order 0 = 0b0000
-            // order 1 = 0b1000
-            // order 2 = 0b1100
-            // order 3 = 0b1110
-            // We do this by starting with all bits set (max_usize). We then shift out
-            // `max_order - order` bits, so when order is 0, all bits are shifted out of
-            // the window. The window is then applied with a bitwise and.
-            const pair_index = (max_usize << (max_order - order)) & ((@as(usize, 1) << max_order) - 1);
-            // * 2 to account for two bits per pair
-            return pair_index * 2;
+        pub fn free(self: Self, allocator: Allocator, index: usize, order: u6) void {
+            const bit_index = alloc_bit_index(index, order);
+
+            // Ensure this allocation even existed in the first place.
+            if (!self.alloc_tracking.isSet(bit_index)) {
+                @panic("Double free");
+            }
+
+            // Combine with sibling, if sibling exists and is free.
+
+            var order_being_merged = order;
+            var block_being_merged = index;
+
+            // Can't merge top order with sibling, as it has no sibling, hence `max_order - 1`
+            while (order_being_merged < max_order - 1) : (order_being_merged += 1) {
+                const sibling = sibling_of(block_being_merged);
+
+                var index_in_free_list = undefined;
+                for (self.next_free[order_being_merged].items, 0..) |block, i| {
+                    if (block == sibling) break;
+                    index_in_free_list = i;
+                } else return; // Couldn't find sibling.
+
+                // We've found the sibling, so now we can merge.
+
+                // Remove sibling from free list
+                self.next_free[order_being_merged].swapRemove(index_in_free_list);
+
+                block_being_merged = @min(block_being_merged, sibling);
+                self.next_free[order_being_merged + 1].append(allocator, block_being_merged);
+            } else {
+                self.next_free[order].append(allocator, index);
+            }
+        }
+
+        const max_order_offset = (@as(usize, 1) << (max_order - 1)) - 1;
+        fn alloc_bit_index(index: usize, order: u6) usize {
+            return (index >> order) + (max_order_offset >> order);
+        }
+
+        fn sibling_of(index: usize, order_size: u6) usize {
+            const mask = (order_size * 2) - 1;
+
+            if (index & mask == 0) {
+                return index + order_size;
+            } else {
+                return index - order_size;
+            }
+        }
+
+        fn get_order_size(order: u6) usize {
+            return @as(usize, 1) << (order + 1);
         }
     };
 }
